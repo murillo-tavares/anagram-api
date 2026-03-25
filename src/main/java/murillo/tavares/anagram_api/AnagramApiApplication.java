12a13ea45514b715c9e7bf049141bd69af263756
Murillo Tavares <murillo.tavares@behoh.com>
2026-03-25 17:29:16 -0300

    first commit

---
+++ src/main/java/murillo/tavares/anagram_api/AnagramApiApplication.java
@@ -0,0 +1,13 @@
+package murillo.tavares.anagram_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnagramApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnagramApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/murillo/tavares/anagram_api/AnagramApiApplicationTests.java
@@ -0,0 +1,13 @@
+package murillo.tavares.anagram_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnagramApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
